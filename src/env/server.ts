a3817b8dcafdb6809586477d8666a399aaef29e8
AriSuseno <arcen.dev@gmail.com>
2026-01-06 10:44:31 +0700

    chore: disable telemetry

--- src/env/server.ts
+++ src/env/server.ts
@@ -1,13 +1,13 @@
-// import 'server-only'
-
 import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(['development', 'production', 'test']),
+    NEXT_TELEMETRY_DISABLED: z.coerce.number().min(0).max(1).default(1),
     DATABASE_URL: z.url().min(1),
     BETTER_AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_TELEMETRY: z.coerce.number().min(0).max(1).default(0),
   },
   experimental__runtimeEnv: process.env,
 })
